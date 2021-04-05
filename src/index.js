961d3688f487616ae14a4763502a11bd5ba589bb
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-04-05 13:36:06 -0400

    Center elements on page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./styles/index.scss";
 import Game from './scripts/Game'
 
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getBoundingClientRect("2d");
+canvas.width = 500;
+canvas.height = 500;
+
 export function newGame() {
     game = new Game();
-    document.getElementById('cat-cam');
+    document.getElementById("canvas");
 }
 
 let game = new Game();
