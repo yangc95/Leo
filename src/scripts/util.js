62f243f7c0d7bdc82901df8e568a1dd9155fa20f
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-07-08 01:39:03 -0400

    Allow user to move with wasd keys

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -3,17 +3,29 @@ class Util {
         document.addEventListener('keydown', e => {
             e.preventDefault();
             switch (e.keyCode) {
-                case 39:
-                   leo.right = true
-                    break
                 case 37:
                     leo.left = true;
                     break
-                case 40:
-                    leo.down = true
+                case 65:
+                    leo.left = true;
                     break
                 case 38:
-                    leo.up = true
+                    leo.up = true;
+                    break
+                case 87:
+                    leo.up = true;
+                    break
+                case 39:
+                    leo.right = true
+                    break
+                case 68:
+                    leo.right = true
+                    break
+                case 40:
+                    leo.down = true;
+                    break
+                case 83:
+                    leo.down = true;
                     break
             }
         });
@@ -21,17 +33,29 @@ class Util {
         document.addEventListener('keyup', e => {
             e.preventDefault();
             switch (e.keyCode) {
+                case 37:
+                    leo.left = false;
+                    break;
+                case 65:
+                    leo.left = false;
+                    break;
+                case 38:
+                    leo.up = false;
+                    break;
+                case 87:
+                    leo.up = false;
+                    break;
                 case 39:
                     leo.right = false;
                     break;
-                case 37:
-                    leo.left = false;
+                case 68:
+                    leo.right = false;
                     break;
                 case 40:
                     leo.down = false;
                     break;
-                case 38:
-                    leo.up = false;
+                case 83:
+                    leo.down = false;
                     break;
             }
         });
