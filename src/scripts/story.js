f4643a8c938db19dd18038e2da283de63c0d6d6c
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-07-07 22:49:49 -0400

    Add cloud gif background

--- src/scripts/story.js
+++ src/scripts/story.js
@@ -5,7 +5,7 @@ class Story {
         this.storyline = {
             // based on the point in the storyline, the map will change
             // map changes include what characters say and where they are located
-            foundPolly: false,
+            foundPolly: false, 
             foundHewert: false 
         };
         // this is a story line object
