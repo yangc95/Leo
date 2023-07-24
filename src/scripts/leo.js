ca70ef82e0f9ca08fff1751f8e7dea18573b999b
Christine Yang <72277593+yangc95@users.noreply.github.com>
2023-07-24 12:07:25 -0400

    Remove comments

--- src/scripts/leo.js
+++ src/scripts/leo.js
@@ -2,13 +2,12 @@ class Leo {
     constructor(x, y, ctx) {
         this.leo = new Image();
         this.leo.src = "src/assets/images/sprite/LeoWalkCycle.png";
-        // state of leo
         this.x = x;
         this.y = y;
         this.ctx = ctx;
 
         this.size = 100;
-        this.dir = 0; // which key is held down
+        this.dir = 0;
         this.trotting = 0;
         this.speed = 0.1;
 
@@ -18,22 +17,13 @@ class Leo {
         this.up = false;
     }
 
-    draw() { // render(ctx, width, height)
-        // console.log("where the heck is leo")
+    draw() {
         let spriteSize = this.size / 4;
         let trot = Math.floor(this.trotting % 4);
         this.ctx.drawImage(this.leo, trot * spriteSize, this.dir * spriteSize, spriteSize, spriteSize, 200, 200, this.size, this.size);   
-        // console.log(this.x + "-------" + this.y)
     }
 
     render() {
-        // const dir = dir[0];
-        // if (dir) {
-        //     if (this.right) { this.trotting += this.speed; }
-        //     if (this.left) { this.trotting -= this.speed; }
-        //     if (this.down) { this.trotting += this.speed; }
-        //     if (this.up) { this.trotting -= this.speed; }      
-        // }
         const dir = {
             "right": 0,
             "left": 1,
@@ -45,22 +35,18 @@ class Leo {
             this.dir = dir["right"];
             this.trotting += this.speed;
             this.x += 3;
-            // console.log("right"+this.x)
         } else if (this.left && this.x > -460) {
             this.dir = dir["left"];
             this.trotting += this.speed;
             this.x -= 3;
-            // console.log("left"+this.x)
         } else if (this.down && this.y < 1925) {
             this.dir = dir["down"];
             this.trotting += this.speed;
             this.y += 3;
-            // console.log("down"+this.y)
         } else if (this.up && this.y > -500) {
             this.dir = dir["up"];
             this.trotting += this.speed;
             this.y -= 3;
-            // console.log("up"+this.y)
         }
     }
 }
