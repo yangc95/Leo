6d956eebd329d5d12ce4a15ce58e5653209bb5b5
Christine Yang <72277593+yangc95@users.noreply.github.com>
2023-07-24 12:03:33 -0400

    Remove comments

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
   ctx.imageSmoothingEnabled = false;
   
   const game = new Game(canvasEl.width, canvasEl.height, ctx);
-  // new GameView(game, ctx).start();
   
   function animate() {
     console.log('animate')
@@ -22,8 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       game.restart();
       return;
     }
-    // list any other conditionals above
-        
+    
     ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
     game.draw();
     game.render();
@@ -40,16 +38,3 @@ document.addEventListener("DOMContentLoaded", () => {
     restart.Buttonstyle.visibility = 'visible';
   });
 });
-
-// export function newGame() {
-//     game = new Game(canvas.width, canvas.height, ctx);
-//     document.getElementById("start-div").style.visibility = "visible";
-//     document.getElementById("end-div").style.visibility = "hidden";
-// }
-
-// window.addEventListener("keydown", e => {
-//     keys[e.keyCode] = true; 
-// });
-// window.addEventListener("keyup", e => {
-//     delete keys[e.keyCode]; 
-// });
