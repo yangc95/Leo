cee9033ecadfeca859ffc48e34fef3913e4693f4
Christine Yang <72277593+yangc95@users.noreply.github.com>
2023-07-24 12:05:55 -0400

    Update houses.js

--- src/scripts/map_ele/houses.js
+++ src/scripts/map_ele/houses.js
@@ -8,31 +8,6 @@ class Houses {
         this.yCollision = this.yPos - this.baseHeight;
         this.house = new Image();
     }
-
-    drawPollysHouse(dx, dy) {
-        const pollysHouse = this.house;
-        pollysHouse.src = "src/assets/images/map_elements/pollysHouse.png";
-
-        const baseWidth = 80;
-        const baseHeight = 120;
-        let leoDx = baseWidth;
-        let leoDy = baseHeight;
-
-        // this.ctx.drawImage(pollysHouse, , 0, baseWidth * 5, baseHeight * 5);
-    }
-
-    drawHewertsHouse(dx, dy) {
-        const hewertsHouse = this.house;
-        hewertsHouse.src = "src/assets/images/map_elements/hewertsHouse.png";
-
-        const baseWidth = 200;
-        const baseHeight = 120;
-        let leoDx = this.xPos - baseWidth;
-        let leoDy = this.yPos - baseHeight;
-
-        // console.log(leoDy)
-        this.ctx.drawImage(hewertsHouse, 0, 0, baseWidth, baseHeight, dx, dy, baseWidth * 3, baseHeight * 3);
-    }
 }
 
 export default Houses;
