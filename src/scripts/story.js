a40de1f7dc0b5b38e4791850e1b79c614b60ec8c
Christine Yang <72277593+yangc95@users.noreply.github.com>
2023-07-24 12:09:18 -0400

    Update story.js

--- src/scripts/story.js
+++ src/scripts/story.js
@@ -3,12 +3,9 @@ class Story {
         this.leo = leo;
         this.map = map;
         this.storyline = {
-            // based on the point in the storyline, the map will change
-            // map changes include what characters say and where they are located
             foundPolly: false, 
             foundHewert: false 
         };
-        // this is a story line object
         this.currentPos = {
             map: true,
             pollysHouse: false,
@@ -17,7 +14,7 @@ class Story {
     }
 
     storylines () {
-    
+        // Storyline here
     }
 }
 
